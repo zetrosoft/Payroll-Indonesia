e9b056d7c444cc74107120dbe2f12860354e2030
dannyaudian <danny.a.pratama@gmail.com>
2025-04-22 10:54:21 +0700

    Update bpjs_payment_summary.js

--- payroll_indonesia/payroll_indonesia/doctype/bpjs_payment_summary/bpjs_payment_summary.js
+++ payroll_indonesia/payroll_indonesia/doctype/bpjs_payment_summary/bpjs_payment_summary.js
@@ -4,13 +4,19 @@ frappe.ui.form.on('BPJS Payment Summary', {
         if(frm.doc.docstatus === 1 && !frm.doc.payment_entry) {
             frm.add_custom_button(__('Generate Payment Entry'), function() {
                 frappe.confirm(
-                    __('Are you sure you want to create Payment Entry?'),
+                    __('Are you sure you want to create Payment Entry for BPJS payment?'),
                     function() {
                         frm.call({
                             doc: frm.doc,
                             method: 'generate_payment_entry',
+                            freeze: true,
+                            freeze_message: __('Generating Payment Entry...'),
                             callback: function(r) {
                                 if(r.message) {
+                                    frappe.show_alert({
+                                        message: __('Payment Entry {0} created', [r.message]),
+                                        indicator: 'green'
+                                    });
                                     frappe.set_route('Form', 'Payment Entry', r.message);
                                 }
                             }
@@ -24,7 +30,7 @@ frappe.ui.form.on('BPJS Payment Summary', {
         if(frm.doc.payment_entry) {
             frm.add_custom_button(__('View Payment Entry'), function() {
                 frappe.set_route('Form', 'Payment Entry', frm.doc.payment_entry);
-            });
+            }, __('Actions'));
         }
     },
     
@@ -32,12 +38,20 @@ frappe.ui.form.on('BPJS Payment Summary', {
         if (frm.is_new()) {
             frm.set_value('posting_date', frappe.datetime.get_today());
         }
+    },
+    
+    validate: function(frm) {
+        calculate_total(frm);
     }
 });
 
 // Component child table calculations
 frappe.ui.form.on('BPJS Payment Component', {
-    amount: function(frm, cdt, cdn) {
+    komponen_add: function(frm) {
+        calculate_total(frm);
+    },
+    
+    amount: function(frm) {
         calculate_total(frm);
     },
     
