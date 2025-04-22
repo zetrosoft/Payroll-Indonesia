b173a21900217566f540ef30455acf5b0250967f
dannyaudian <danny.a.pratama@gmail.com>
2025-04-22 10:24:39 +0700

    Update validations.py

--- payroll_indonesia/payroll_indonesia/validations.py
+++ payroll_indonesia/payroll_indonesia/validations.py
@@ -3,15 +3,33 @@ from frappe import _
 
 @frappe.whitelist()
 def validate_employee_golongan(jabatan, golongan):
-    if not (jabatan and golongan):
-        return
+    """
+    Validate that employee's golongan level does not exceed the maximum allowed for their jabatan
+    
+    Args:
+        jabatan (str): The jabatan (position) code
+        golongan (str): The golongan (grade) code
+        
+    Raises:
+        frappe.ValidationError: If golongan level exceeds maximum allowed level
+    """
+    if not jabatan:
+        frappe.throw(_("Jabatan is required"))
+    
+    if not golongan:
+        frappe.throw(_("Golongan is required"))
         
     max_golongan = frappe.db.get_value('Jabatan', jabatan, 'max_golongan')
     if not max_golongan:
-        return
+        frappe.throw(_("Maximum Golongan not set for Jabatan {0}").format(jabatan))
         
     max_level = frappe.db.get_value('Golongan', max_golongan, 'level')
+    if not max_level:
+        frappe.throw(_("Level not set for Golongan {0}").format(max_golongan))
+        
     current_level = frappe.db.get_value('Golongan', golongan, 'level')
+    if not current_level:
+        frappe.throw(_("Level not set for Golongan {0}").format(golongan))
     
     if current_level > max_level:
         frappe.throw(
