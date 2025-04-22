4fd66b24951b8b9abba0c47dd7944943cf084983
dannyaudian <danny.a.pratama@gmail.com>
2025-04-22 10:54:45 +0700

    Update bpjs_payment_summary.py

--- payroll_indonesia/payroll_indonesia/doctype/bpjs_payment_summary/bpjs_payment_summary.py
+++ payroll_indonesia/payroll_indonesia/doctype/bpjs_payment_summary/bpjs_payment_summary.py
@@ -5,13 +5,36 @@
 import frappe
 from frappe import _
 from frappe.model.document import Document
-from frappe.utils import today, flt
+from frappe.utils import today, flt, get_formatted_currency
 
 class BPJSPaymentSummary(Document):
     def validate(self):
+        self.validate_company()
+        self.validate_components()
+        self.calculate_total()
         self.validate_total()
         self.validate_supplier()
-        self.calculate_total()
+    
+    def validate_company(self):
+        """Validate company and its default accounts"""
+        if not self.company:
+            frappe.throw(_("Company is mandatory"))
+            
+        # Check default accounts
+        company_doc = frappe.get_doc("Company", self.company)
+        if not company_doc.default_bank_account:
+            frappe.throw(_("Default Bank Account not set for Company {0}").format(self.company))
+        if not company_doc.default_payable_account:
+            frappe.throw(_("Default Payable Account not set for Company {0}").format(self.company))
+    
+    def validate_components(self):
+        """Validate BPJS components"""
+        if not self.komponen:
+            frappe.throw(_("At least one BPJS component is required"))
+            
+        for d in self.komponen:
+            if not d.amount or d.amount <= 0:
+                frappe.throw(_("Amount must be greater than 0 for component {0}").format(d.idx))
     
     def calculate_total(self):
         """Calculate total from components"""
@@ -25,9 +48,8 @@ class BPJSPaymentSummary(Document):
     def validate_supplier(self):
         """Validate BPJS supplier exists"""
         if not frappe.db.exists("Supplier", "BPJS"):
-            frappe.throw(
-                _("Supplier 'BPJS' does not exist. Please create it first.")
-            )
+            from .bpjs_payment_validation import create_bpjs_supplier
+            create_bpjs_supplier()
     
     def on_submit(self):
         """Set status to Submitted"""
@@ -36,7 +58,9 @@ class BPJSPaymentSummary(Document):
     def on_cancel(self):
         """Reset status to Draft"""
         if self.payment_entry:
-            frappe.throw(_("Cannot cancel document with linked Payment Entry"))
+            pe_status = frappe.db.get_value("Payment Entry", self.payment_entry, "docstatus")
+            if pe_status and int(pe_status) == 1:
+                frappe.throw(_("Cannot cancel document with submitted Payment Entry"))
         self.status = "Draft"
     
     @frappe.whitelist()
@@ -44,9 +68,6 @@ class BPJSPaymentSummary(Document):
         """Generate Payment Entry for BPJS payment"""
         if self.payment_entry:
             frappe.throw(_("Payment Entry already exists"))
-            
-        if not self.company:
-            frappe.throw(_("Company is required"))
             
         if self.docstatus != 1:
             frappe.throw(_("Document must be submitted first"))
@@ -70,25 +91,26 @@ class BPJSPaymentSummary(Document):
             pe.reference_doctype = self.doctype
             pe.reference_name = self.name
             
-            # Add custom remarks
-            pe.remarks = f"BPJS Payment for {self.name}"
+            # Add custom remarks with components
+            components_text = "\n".join([
+                f"- {d.component}: {get_formatted_currency(d.amount, pe.company)}"
+                for d in self.komponen
+            ])
+            pe.remarks = (
+                f"BPJS Payment for {self.name}\n"
+                f"Components:\n{components_text}"
+            )
             
             # Save and submit
             pe.insert()
             pe.submit()
             
             # Update this document
-            self.payment_entry = pe.name
-            self.status = "Paid"
-            self.save()
-            
-            frappe.msgprint(
-                _("Payment Entry {0} has been created").format(
-                    frappe.bold(pe.name)
-                )
-            )
+            self.db_set('payment_entry', pe.name)
+            self.db_set('status', 'Paid')
             
             return pe.name
             
         except Exception as e:
-            frappe.throw(_("Error creating Payment Entry: {0}").format(str(e)))
+            frappe.msgprint(_("Error creating Payment Entry"))
+            frappe.throw(str(e))
