63a3a267f79c53a4dee73f6c4b3164b6749247c8
dannyaudian <danny.a.pratama@gmail.com>
2025-04-28 13:23:54 +0700

    Update __init__.py

--- payroll_indonesia/override/salary_slip/__init__.py
+++ payroll_indonesia/override/salary_slip/__init__.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2025, PT. Innovasi Terbaik Bangsa and contributors
 # For license information, please see license.txt
-# Last modified: 2025-04-27 11:24:03 by dannyaudian
-
-# Import all modules to make them available
-from . import base, tax_calculator, bpjs_calculator, ter_calculator
-from . import tax_summary_creator, bpjs_summary_creator, ter_table_creator
+# Last modified: 2025-04-28 06:21:00 by dannyaudian
 
 # Import the main controller class
 from .controller import IndonesiaPayrollSalarySlip
 
 # Export the class for direct imports
 __all__ = ['IndonesiaPayrollSalarySlip']
+
+# Note: We avoid importing other modules here to prevent circular imports
+# Modules are imported directly where needed
