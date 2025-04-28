4814deb653bc43964d039c9699f391dba96e846b
dannyaudian <danny.a.pratama@gmail.com>
2025-04-28 19:55:12 +0700

    fix

--- payroll_indonesia/payroll_indonesia/doctype/bpjs_payment_summary/__init__.py
+++ payroll_indonesia/payroll_indonesia/doctype/bpjs_payment_summary/__init__.py
@@ -2,10 +2,16 @@
 # Copyright (c) 2025, PT. Innovasi Terbaik Bangsa and contributors
 # For license information, please see license.txt
 
-# Import fungsi-fungsi penting yang digunakan oleh template Jinja atau sistem lain
+# Import fungsi-fungsi dari bpjs_payment_api.py untuk memudahkan akses
 from .bpjs_payment_api import (
+    get_bpjs_suppliers,
     get_summary_for_period,
     get_employee_bpjs_details,
-    create_payment_entry,
-    get_bpjs_suppliers
+    create_payment_entry
+)
+
+# Import fungsi-fungsi lain yang mungkin dibutuhkan
+from .bpjs_payment_integration import (
+    create_from_salary_slip,
+    update_on_salary_slip_cancel
 )
