1ce85a966d2bfae9b7ac7dfacde0241c96d39f42
dannyaudian <danny.a.pratama@gmail.com>
2025-04-30 15:09:09 +0700

    Update __init__.py

--- payroll_indonesia/override/salary_slip/__init__.py
+++ payroll_indonesia/override/salary_slip/__init__.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2025, PT. Innovasi Terbaik Bangsa and contributors
 # For license information, please see license.txt
-# Last modified: 2025-04-30 07:51:52 by dannyaudian
+# Last modified: 2025-04-30 08:05:35 by dannyaudiandesk
 
-# Export the main salary slip controller class and utility functions
-from payroll_indonesia.override.salary_slip import (
-    IndonesiaPayrollSalarySlip,
+# Import directly from the controller module
+from .controller import IndonesiaPayrollSalarySlip
+from .controller import (
     setup_fiscal_year_if_missing,
     process_salary_slips_batch,
     check_fiscal_year_setup,
-    clear_caches,
-    debug_log
+    clear_caches
 )
+
+# Import the debug_log function from the bpjs_calculator module
+# to solve the circular import issue
+from payroll_indonesia.calculations.bpjs_calculator import debug_log
 
 # Export for direct imports
 __all__ = [
@@ -19,9 +22,9 @@ __all__ = [
     'setup_fiscal_year_if_missing',
     'process_salary_slips_batch',
     'check_fiscal_year_setup',
-    'debug_log',
-    'clear_caches'
+    'clear_caches',
+    'debug_log'
 ]
 
-# Note: We selectively import functions to prevent circular imports
-# BPJS-specific functionality is now imported from payroll_indonesia.calculations.bpjs_calculator
+# Note: We're importing debug_log directly from bpjs_calculator
+# to prevent circular import issues between salary_slip and salary_slip_functions
